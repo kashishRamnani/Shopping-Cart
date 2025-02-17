88f5094eaac3519cedc135ae730012b68d8a4dca
kashish ramnani <kashishramnani337@gmail.com>
2025-02-17 13:32:01 +0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
- 
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'redux', 'react-redux'], // This will create a separate chunk for these libraries
+        },
+      },
+    },
+  },
+  base: "/shopping-cart", // Your desired base path for deployment
 })
